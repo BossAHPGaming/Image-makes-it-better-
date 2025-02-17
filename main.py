f0a86d2f1b2cf606198e1ee9f073850925fe432d
BossAHPGaming <aaravhpatel210@gmail.com>
2025-02-17 18:46:41 +0530

    Images

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from PIL import Image, ImageTk
 
 root = Tk()
 root.title("THE ONE PEICE IS REAL")
-root.geometry('1000x800')
+root.geometry('400x300')
 
 upload = Image.open("ONE PIECE IS REAL.jpg")
 
 image = ImageTk.PhotoImage(upload)
 
-label = Label(root, image=image, height=700, width= 650)
+label = Label(root, image=image, height=350, width= 300)
 label.place(x=50, y=0)
 label2 = Label(root, text="THE ONE PICE IS REAL")
 label2.place(x=40, y=360)
